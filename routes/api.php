50dfc6f29be0dc0f9c813a59d14f322e1b551702
Dev.Alysonh <dev.alysonh@gmail.com>
2024-09-26 23:05:14 -0300

    cria a rota para criação de produtos.

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(CategoryController::class)->group(function () {
     Route::post('/categories', 'store');
+});
+
+Route::controller(ProductController::class)->group(function () {
+    Route::post('/products', 'store');
 });
