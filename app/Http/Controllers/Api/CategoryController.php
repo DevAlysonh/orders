31af14c7451a15bd1dede522b4153013ad9ca903
Dev.Alysonh <dev.alysonh@gmail.com>
2024-09-28 12:46:15 -0300

    Refatorei o controller para usar a nova responsefactory

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -2,72 +2,47 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Factories\ResponseFactory;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\CreateCategoryRequest;
 use App\Services\Api\CategoryService;
+use App\Traits\SwaggerDocs\CategoryControllerDocs;
 use Illuminate\Http\JsonResponse;
-use InvalidArgumentException;
-use OpenApi\Annotations as OA;
+use Illuminate\Http\Response;
+use Throwable;
 
 class CategoryController extends Controller
 {
+    use CategoryControllerDocs;
     public function __construct(protected CategoryService $categoryService)
     {
     }
 
-    /**
-     * @OA\Post(
-     *     path="/api/categories",
-     *     summary="Registrar uma nova categoria.",
-     *     description="Cria uma nova categoria de produtos. O campo nome é obrigatório e deve ser uma string válida.",
-     *     tags={"Categorias"},
-     *     @OA\RequestBody(
-     *         required=true,
-     *         @OA\JsonContent(
-     *             required={"name"},
-     *             @OA\Property(
-     *                 property="name",
-     *                 description="Nome da categoria",
-     *                 type="string"
-     *             )
-     *         )
-     *     ),
-     *     @OA\Response(
-     *         response=201,
-     *         description="Categoria registrada.",
-     *         @OA\JsonContent(
-     *             @OA\Property(property="msg", type="string", example="Categoria cadastrada."),
-     *             @OA\Property(property="category", type="object",
-     *                 @OA\Property(property="id", type="integer", example=5),
-     *                 @OA\Property(property="name", type="string", example="Lanches")
-     *             )
-     *         )
-     *     ),
-     *     @OA\Response(
-     *         response=400,
-     *         description="Requisição inválida, campos inválidos.",
-     *         @OA\JsonContent(
-     *             @OA\Property(property="errors", type="object",
-     *                 @OA\Property(property="name", type="array", @OA\Items(type="string", example="O campo name é orbigatório."))
-     *             )
-     *         )
-     *     )
-     * )
-     */
     public function store(CreateCategoryRequest $request): JsonResponse
     {
         try {
             $category = $this->categoryService
                 ->create($request->validated());
 
-            return response()->json([
-                'msg' => $this->categoryService->getLastMessage(),
-                'category' => $category
-            ], 201);
-        } catch (InvalidArgumentException $e) {
-            return response()->json([
-                'error' => $this->categoryService->getLastMessage(),
-            ], 400);
+            return response()->json(
+                ResponseFactory::make(
+                    ResponseFactory::SUCCESS,
+                    $this->categoryService->getLastMessage(),
+                    Response::HTTP_CREATED,
+                    $category
+                ),
+                Response::HTTP_CREATED
+            );
+        } catch (Throwable $e) {
+            return response()->json(
+                ResponseFactory::make(
+                    ResponseFactory::ERROR,
+                    'Erro interno do servidor.',
+                    Response::HTTP_INTERNAL_SERVER_ERROR,
+                    [ResponseFactory::INTERNAL_ERROR_MESSAGE]
+                ),
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
         }
     }
 }
