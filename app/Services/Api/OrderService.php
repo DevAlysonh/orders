9ce0a5d13c79f90d379e752cd2afcf3e05cc89ae
Dev.Alysonh <dev.alysonh@gmail.com>
2024-09-28 18:33:22 -0300

    Criando a lógica para criar um novo pedido, adicionar produtos, e calcular o valor total do pedido.

--- app/Services/Api/OrderService.php
+++ app/Services/Api/OrderService.php
@@ -2,12 +2,41 @@
 
 namespace App\Services\Api;
 
+use App\Models\Api\Order;
+
 class OrderService
 {
     protected string $lastMessage;
 
-    public function newOrder($products)
+    public function newOrder(array $products): Order
     {
-        return null;
+        $order = Order::create([
+            'status' => Order::STATUS_OPEN,
+            'total' => $this->handleOrderTotalPrice($products)
+        ]);
+
+        $this->addProductsToOrder($order, $products);
+
+        return $order;
+    }
+
+    private function addProductsToOrder(Order $order, array $products): void
+    {
+        foreach ($products as $product) {
+            $order->products()->attach($product['product_id'], [
+                'quantity' => $product['quantity'],
+                'price' => $product['price'] * 100
+            ]);
+        }
+    }
+
+    private function handleOrderTotalPrice(array $products): float
+    {
+        $total = 0.0;
+        foreach ($products as $product) {
+            $total += $product['price'] * $product['quantity'];
+        }
+
+        return $total;
     }
 }
