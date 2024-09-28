464f275e6012a28442522474a11471194707861f
Dev.Alysonh <dev.alysonh@gmail.com>
2024-09-28 17:57:39 -0300

    define mutattors para o atributo total

--- app/Models/Api/Order.php
+++ app/Models/Api/Order.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Api;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -10,10 +11,28 @@ class Order extends Model
 {
     use HasFactory;
 
+    public const STATUS_OPEN = 'open';
+    public const STATUS_APPROVED = 'approved';
+    public const STATUS_FINISHED = 'finished';
+    public const STATUS_CANCELLED = 'cancelled';
+
     protected $fillable = [
         'status',
         'total'
     ];
+
+    protected function total(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => number_format(
+                $value / 100,
+                2,
+                '.',
+                ' '
+            ),
+            set: fn (float $value) => $value * 100,
+        );
+    }
 
     public function products(): BelongsToMany
     {
