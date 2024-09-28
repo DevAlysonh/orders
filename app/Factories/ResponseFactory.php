b4ba3ebbad29ca6cb97ee89396909af0d9860a18
Dev.Alysonh <dev.alysonh@gmail.com>
2024-09-27 21:13:32 -0300

    Creates Response Factory class

---
+++ app/Factories/ResponseFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Factories;
+
+
+use App\Http\Responses\ResponseError;
+use App\Http\Responses\ResponseSuccess;
+
+class ResponseFactory
+{
+    public const SUCCESS = 'success';
+    public const ERROR = 'error';
+
+    public static function make(
+        string $status,
+        string $message,
+        int $statusCode,
+        array $data
+    ): array {
+        return match ($status) {
+            self::SUCCESS => (new ResponseSuccess())->build($message, $statusCode, $data),
+            self::ERROR => (new ResponseError())->build($message, $statusCode, $data),
+            default => []
+        };
+    }
+}
