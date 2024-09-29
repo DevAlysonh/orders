a91da6379da45bcb7cc15420ed82c8625a175934
Dev.Alysonh <dev.alysonh@gmail.com>
2024-09-28 22:00:21 -0300

    Gerando a documentação do endpoint

--- app/Traits/SwaggerDocs/OrderControllerDocs.php
+++ app/Traits/SwaggerDocs/OrderControllerDocs.php
@@ -105,7 +105,7 @@ trait OrderControllerDocs
 
     /**
      * @OA\Get(
-     *     path="/api/orders/",
+     *     path="/api/orders/list/",
      *     summary="Retorna os pedidos já registrados no sistema",
      *     description="Retorna uma lista contendo os pedidos registrados no sistema com paginação.",
      *     tags={"Pedidos"},
@@ -158,6 +158,72 @@ trait OrderControllerDocs
      * )
      */
     public function orderListDocs()
+    {
+        //
+    }
+
+    /**
+     * @OA\Get(
+     *     path="/api/orders/{order}",
+     *     summary="Listar detalhes de um pedido",
+     *     description="Retorna os detalhes de um pedido específico com base no ID do pedido.",
+     *     tags={"Pedidos"},
+     *     @OA\Parameter(
+     *         name="order_id",
+     *         in="path",
+     *         required=true,
+     *         description="ID do pedido",
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Detalhes do pedido obtidos com sucesso.",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="status", type="string", example="success"),
+     *             @OA\Property(property="message", type="string", example="Detalhes do pedido."),
+     *             @OA\Property(property="data", type="object",
+     *                 @OA\Property(property="order_id", type="integer", example=4),
+     *                 @OA\Property(property="order_status", type="string", example="open"),
+     *                 @OA\Property(property="total_cost", type="string", example="5.00"),
+     *                 @OA\Property(property="products", type="array",
+     *                     @OA\Items(type="object",
+     *                         @OA\Property(property="product_id", type="integer", example=1),
+     *                         @OA\Property(property="product_name", type="string", example="Pizza Marguerita"),
+     *                         @OA\Property(property="price", type="string", example="2.50"),
+     *                         @OA\Property(property="quantity", type="integer", example=2)
+     *                     )
+     *                 )
+     *             ),
+     *             @OA\Property(property="status_code", type="integer", example=200)
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Pedido não encontrado.",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="status", type="string", example="error"),
+     *             @OA\Property(property="message", type="string", example="O pedido não foi localizado."),
+     *             @OA\Property(property="errors", type="array", @OA\Items()),
+     *             @OA\Property(property="status_code", type="integer", example=404)
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Erro interno do servidor.",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="status", type="string", example="error"),
+     *             @OA\Property(property="message", type="string", example="Erro interno do servidor."),
+     *             @OA\Property(property="errors", type="array",
+     *                 @OA\Items(type="string", example="Ocorreu um erro ao processar sua solicitação, tente novamente em instantes.")
+     *             ),
+     *             @OA\Property(property="status_code", type="integer", example=500)
+     *         )
+     *     )
+     * )
+     */
+    public function orderShowDocs()
     {
         //
     }
