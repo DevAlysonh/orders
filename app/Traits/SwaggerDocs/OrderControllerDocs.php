7089d20ed9dbda21aa8687f8cf24b63678dde577
Dev.Alysonh <dev.alysonh@gmail.com>
2024-09-28 23:10:16 -0300

    gerando as documentações no swagger

--- app/Traits/SwaggerDocs/OrderControllerDocs.php
+++ app/Traits/SwaggerDocs/OrderControllerDocs.php
@@ -8,7 +8,7 @@ trait OrderControllerDocs
 {
     /**
      * @OA\Post(
-     *     path="/orders",
+     *     path="/api/orders",
      *     summary="Registrar um pedido",
      *     description="Essa rota registra um pedido com base nos produtos fornecidos.",
      *     tags={"Pedidos"},
@@ -169,7 +169,7 @@ trait OrderControllerDocs
      *     description="Retorna os detalhes de um pedido específico com base no ID do pedido.",
      *     tags={"Pedidos"},
      *     @OA\Parameter(
-     *         name="order_id",
+     *         name="order",
      *         in="path",
      *         required=true,
      *         description="ID do pedido",
@@ -224,6 +224,103 @@ trait OrderControllerDocs
      * )
      */
     public function orderShowDocs()
+    {
+        //
+    }
+
+    /**
+     * @OA\Patch(
+     *     path="/api/orders/{order}",
+     *     summary="Atualiza o status de um pedido",
+     *     description="Atualiza o status de um pedido existente. O status deve ser um dos seguintes valores: 'open', 'approved', 'finished', ou 'cancelled'. O campo 'total' não deve ser enviado.",
+     *     tags={"Pedidos"},
+     *     @OA\Parameter(
+     *         name="order",
+     *         in="path",
+     *         description="ID do pedido que será atualizado",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"status"},
+     *             @OA\Property(
+     *                 property="status",
+     *                 type="string",
+     *                 description="Novo status do pedido",
+     *                 enum={"open", "approved", "finished", "cancelled"}
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Pedido atualizado com sucesso",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="status", type="string", example="success"),
+     *             @OA\Property(property="message", type="string", example="Pedido atualizado"),
+     *             @OA\Property(
+     *                 property="data",
+     *                 type="object",
+     *                 @OA\Property(property="order_id", type="integer", example=4),
+     *                 @OA\Property(property="order_status", type="string", example="approved"),
+     *                 @OA\Property(property="total_cost", type="string", example="0.07"),
+     *                 @OA\Property(
+     *                     property="products",
+     *                     type="array",
+     *                     @OA\Items(
+     *                         type="object",
+     *                         @OA\Property(property="product_id", type="integer", example=1),
+     *                         @OA\Property(property="product_name", type="string", example="Pizza Marguerita"),
+     *                         @OA\Property(property="price", type="string", example="2.50"),
+     *                         @OA\Property(property="quantity", type="integer", example=2)
+     *                     )
+     *                 )
+     *             ),
+     *             @OA\Property(property="status_code", type="integer", example=200)
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Pedido não encontrado",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="status", type="string", example="error"),
+     *             @OA\Property(property="message", type="string", example="Pedido não encontrado"),
+     *             @OA\Property(property="errors", type="array", @OA\Items(type="string")),
+     *             @OA\Property(property="status_code", type="integer", example=404)
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Erro interno do servidor",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="status", type="string", example="error"),
+     *             @OA\Property(property="message", type="string", example="Erro interno do servidor."),
+     *             @OA\Property(
+     *                 property="errors",
+     *                 type="array",
+     *                 @OA\Items(type="string", example="Ocorreu um erro ao processar sua solicitação, tente novamente em instantes")
+     *             ),
+     *             @OA\Property(property="status_code", type="integer", example=500)
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Erro de validação",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="status", type="string", example="error"),
+     *             @OA\Property(property="message", type="string", example="Erro de validação."),
+     *             @OA\Property(
+     *                 property="errors",
+     *                 type="array",
+     *                 @OA\Items(type="string", example="O campo status é obrigatório.")
+     *             ),
+     *             @OA\Property(property="status_code", type="integer", example=422)
+     *         )
+     *     )
+     * )
+     */
+    public function orderUpdateDocs()
     {
         //
     }
